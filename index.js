2cf3f4e359ba5b1194b122d753a1c8e03c62936c
danknjoroge <dank8505@gmail.com>
2021-11-17 16:34:50 +0300

    It will modify the form

--- index.js
+++ index.js
@@ -24,3 +24,38 @@ $(document).ready(function (){
     
 
 });
+
+
+
+function validatePhoneNumber(input_str) {
+    var p = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+    return p.test(input_str);
+}
+function validateForm(event){
+    var phone = document.getElementById('phone').value;
+    if (!validatePhoneNumber(phone)) {
+        document.getElementById('error').classList.remove('hidden');
+    } else {
+        document.getElementById('error').classList.add('hidden');
+        alert("Validation successful")
+  
+       event.preventDefault();
+}
+document.getElementById('myform').addEventListener('submit', validateForm)};
+
+
+
+function phonenumber(inputtxt)
+{
+  var phoneno = /^\d{10}$/;
+  if(inputtxt.value.match(phoneno))
+  {
+      return true;
+  }
+  else
+  {
+     alert("Not a valid Phone Number");
+     return false;
+  }
+  }
