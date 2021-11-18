8e9fd102bddd4aea781d4d00c91a07abec970b5b
danknjoroge <dank8505@gmail.com>
2021-11-18 08:13:16 +0300

    It will modify the form

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const isValidEmail = (email) =>{
+    const re = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+    return re.test(String(email).toLowerCase());
+}
 
-$(document).ready(function (){
-    const email = document.getElementById('email');
+const isValidPhone = (phone) =>{
+    const re = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+    return re.test(String(phone).toLowerCase());
+}
+
+const email = document.getElementById('email');
     email.addEventListener('blur', ()=>{
         let regex = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
         let x = email.value;
@@ -21,41 +29,3 @@ $(document).ready(function (){
             return false;
         }
     });
-    
-
-});
-
-
-
-function validatePhoneNumber(input_str) {
-    var p = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-
-    return p.test(input_str);
-}
-function validateForm(event){
-    var phone = document.getElementById('phone').value;
-    if (!validatePhoneNumber(phone)) {
-        document.getElementById('error').classList.remove('hidden');
-    } else {
-        document.getElementById('error').classList.add('hidden');
-        alert("Validation successful")
-  
-       event.preventDefault();
-}
-document.getElementById('myform').addEventListener('submit', validateForm)};
-
-
-
-function phonenumber(inputtxt)
-{
-  var phoneno = /^\d{10}$/;
-  if(inputtxt.value.match(phoneno))
-  {
-      return true;
-  }
-  else
-  {
-     alert("Not a valid Phone Number");
-     return false;
-  }
-  }
